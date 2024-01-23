29b60c3a62ff6d9c739e7b58562e57b535231d04
yunii23 <67429918+yunii23@users.noreply.github.com>
2024-01-24 01:30:34 +0900

    1차원 배열 - 개수 세기 완료

--- Baekjoon/One_Array/10807.js
+++ Baekjoon/One_Array/10807.js
@@ -1,1 +1,17 @@
 // 개수 세기
+
+const fs = require('fs');
+const input = fs.readFileSync("example.txt").toString().trim().split(/\n/);
+// 제출시 /dev/stdin
+
+const n = parseInt(input[0]);
+const arr = input[1].trim().split(/\s/);
+const v = input[2];
+
+let answer = 0;
+
+for(let i=0; i<arr.length; i++) {
+    if(arr[i] == v) answer ++;
+}
+
+console.log(answer);
