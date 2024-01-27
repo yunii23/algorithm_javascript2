3270f2740ed876840f82b529e339e7ba534c0bcb
yunii23 <67429918+yunii23@users.noreply.github.com>
2024-01-28 02:04:47 +0900

    1차원 배열 - 공 바꾸기 완료

--- Baekjoon/One_Array/10813.js
+++ Baekjoon/One_Array/10813.js
@@ -1,2 +1,24 @@
 // 공 바꾸기
 
+const fs = require('fs');
+const input = fs.readFileSync("example.txt").toString().trim().split(/\n/);
+// 제출시 /dev/stdin
+
+const [n, m] = input[0].trim().split(" ");
+let basket = new Array(n);
+
+for(let i=0; i<n; i++) {
+    basket[i] = i+1;
+}
+
+for(let i=0; i<m; i++) {
+    let [a, b] = input[i+1].trim().split(" ");
+    let a_basket = basket[a-1];
+
+    basket[a-1] = basket[b-1];
+    basket[b-1] = a_basket;
+}
+
+let answer = basket.join(" ");
+
+console.log(answer);
