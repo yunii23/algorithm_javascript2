2b7e2b401495026ada8da54995b9e05fb41cce44
yunii23 <67429918+yunii23@users.noreply.github.com>
2024-01-31 23:49:59 +0900

    심화1 - 팰린드롬인지 확인하기 완료

--- Baekjoon/Deepen1/10988.js
+++ Baekjoon/Deepen1/10988.js
@@ -4,3 +4,29 @@ const fs = require('fs');
 const input = fs.readFileSync("example.txt").toString().trim();
 // 제출시 /dev/stdin
 
+let word = [...input];
+let j = word.length - 1;
+let answer = 1;
+
+// 홀수
+if(word.length % 2 != 0) {
+    for(let i=0; i<word.length/2; i++) {
+        if(word[i] != word[j]) {
+            answer = 0;
+            break;
+        }
+        j--;
+    }
+}
+// 짝수
+else {
+    for(let i=0; i<word.length/2; i++) {
+        if(word[i] != word[j]) {
+            answer = 0;
+            break;
+        }
+        j--;
+    }
+}
+
+console.log(answer);
