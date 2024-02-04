bcbda43e85dbb1087d17dc09e32c364b67a97357
yunii23 <67429918+yunii23@users.noreply.github.com>
2024-02-04 23:29:31 +0900

    일반 수학1 - 진법 변환 완료

--- Baekjoon/Math1/2745.js
+++ Baekjoon/Math1/2745.js
@@ -1,1 +1,10 @@
 // 진법 변환
+
+const fs = require('fs');
+const input = fs.readFileSync("example.txt").toString().trim().split(/\s/);
+// 제출시 /dev/stdin
+
+let n = input[0];
+let b = input[1];
+
+console.log(parseInt(n, b));
