81469f4ef59d5f99828c44a68fcb21953a66828b
yunii23 <67429918+yunii23@users.noreply.github.com>
2024-02-06 01:50:00 +0900

    2차원 배열 - 색종이 완료

--- Baekjoon/Two_Array/2563.js
+++ Baekjoon/Two_Array/2563.js
@@ -6,3 +6,27 @@ const input = fs.readFileSync("example.txt").toString().trim().split(/\n/);
 
 const n = parseInt(input[0]);
 
+const arr = Array.from(new Array(100), () => new Array(100).fill(false));
+
+for(let i=1; i<input.length; i++) {
+    let temp = input[i].trim().split(" ");
+    let x = parseInt(temp[0]); // 열
+    let y = parseInt(temp[1]); // 행
+    
+    for(let j=y; j<y+10; j++) {
+        for(let k=x; k<x+10; k++) {
+            arr[j][k] = true;
+        }
+    }
+}
+
+let sum = 0;
+for(let i=0; i<arr.length; i++) {
+    for(let j=0; j<arr[i].length; j++) {
+        if(arr[i][j] == true) {
+            sum++;
+        } 
+    }
+}
+
+console.log(sum);
