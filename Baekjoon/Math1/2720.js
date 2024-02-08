bf4eb2fa715b70fbabec5924a291fffb493b005d
yunii23 <67429918+yunii23@users.noreply.github.com>
2024-02-08 23:30:35 +0900

    일반 수학1 - 세탁소 사장 동혁 완료

--- Baekjoon/Math1/2720.js
+++ Baekjoon/Math1/2720.js
@@ -4,3 +4,32 @@ const fs = require('fs');
 const input = fs.readFileSync("example.txt").toString().trim().split(/\n/);
 // 제출시 /dev/stdin
 
+let t = parseInt(input[0]);
+
+for(let i=1; i<=t; i++) {
+    let temp = input[i] / 100;
+    let result = '';
+    let answer = '';
+    
+    result = parseInt(temp / 0.25);
+    answer = result + " ";
+    temp -= result * 0.25;
+    temp = temp.toFixed(2);
+
+    result = parseInt(temp / 0.10);
+    answer += result + " ";
+    temp -= result * 0.10;
+    temp = temp.toFixed(2);
+
+    result = parseInt(temp / 0.05);
+    answer += result + " ";
+    temp -= result * 0.05;
+    temp = temp.toFixed(2);
+
+    result = parseInt(temp / 0.01);
+    answer += result;
+    temp -= result * 0.01;
+    temp = temp.toFixed(2);
+
+    console.log(answer);
+}
