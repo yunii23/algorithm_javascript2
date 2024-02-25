916d43c379b0018279376aeeb7243398803042b6
yunii23 <67429918+yunii23@users.noreply.github.com>
2024-02-25 22:32:04 +0900

    집합과 맵 - 회사에 있는 사람 완료

--- Baekjoon/Set_Map/7785.js
+++ Baekjoon/Set_Map/7785.js
@@ -1,1 +1,21 @@
 // 회사에 있는 사람
+
+const fs = require('fs');
+const input = fs.readFileSync("example.txt").toString().trim().split(/\n/);
+// 제출시 /dev/stdin
+
+let n = parseInt(input[0]);
+let map = new Map();
+let answer = [];
+
+for(let i=1; i<input.length; i++) {
+    let temp = input[i].trim().split(" ");
+    map.set(temp[0], temp[1]);
+}
+
+for(let [k, v] of map) {
+    if(v == "enter") answer.push(k);
+}
+
+answer.sort((a, b) => a > b ? -1 : 1);
+console.log(answer.join("\n"));
