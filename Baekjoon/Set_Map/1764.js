b442ce2c48b4c9a47970d50a59dc0d9fee6f8f8d
yunii23 <67429918+yunii23@users.noreply.github.com>
2024-02-27 01:36:49 +0900

    집합과 맵 - 듣보잡 완료

--- Baekjoon/Set_Map/1764.js
+++ Baekjoon/Set_Map/1764.js
@@ -1,1 +1,23 @@
 // 듣보잡
+
+const fs = require('fs');
+const input = fs.readFileSync("example.txt").toString().trim().split(/\n/);
+// 제출시 /dev/stdin
+
+let temp = input[0].trim().split(" ").map(Number);
+let n = temp[0];
+let answer = [];
+let map = new Map();
+
+for(let i=1; i<=n; i++) {
+    map.set(input[i], 0);
+}
+
+for(let i=n+1; i<input.length; i++) {
+    if(map.has(input[i]) == true) answer.push(input[i]);
+}
+
+answer.sort((a, b) => a > b ? 1 : -1);
+
+console.log(answer.length);
+console.log(answer.join("\n"));
