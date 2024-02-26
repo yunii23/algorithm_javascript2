69b9169d76447bdd6db71534e851eb91f52017ce
yunii23 <67429918+yunii23@users.noreply.github.com>
2024-02-26 22:57:58 +0900

    집합과 맵 - 숫자 카드2 완료

--- Baekjoon/Set_Map/10816.js
+++ Baekjoon/Set_Map/10816.js
@@ -1,1 +1,24 @@
 // 숫자 카드2
+
+const fs = require('fs');
+const input = fs.readFileSync("example.txt").toString().trim().split(/\n/);
+// 제출시 /dev/stdin
+
+let n = parseInt(input[0]);
+let m = parseInt(input[2]);
+let card = input[1].trim().split(" ").map(Number);
+let have = input[3].trim().split(" ").map(Number);
+let answer= [];
+let map = new Map();
+
+for(let i=0; i<card.length; i++) {
+    if(map.has(card[i]) == true) map.set(card[i], map.get(card[i]) + 1);
+    else map.set(card[i], 1);
+}
+
+for(let i=0; i<have.length; i++) {
+    if(map.has(have[i]) == true) answer.push(map.get(have[i]));
+    else answer.push(0);
+}
+
+console.log(answer.join(" "));
