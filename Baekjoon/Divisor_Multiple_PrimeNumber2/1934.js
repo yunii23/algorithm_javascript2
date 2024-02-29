2b02c64b9cc0e2ad5938fd0e0605c7e92737423a
yunii23 <67429918+yunii23@users.noreply.github.com>
2024-03-01 02:02:13 +0900

    약수, 배수와 소수2 - 최소공배수 완료

--- Baekjoon/Divisor_Multiple_PrimeNumber2/1934.js
+++ Baekjoon/Divisor_Multiple_PrimeNumber2/1934.js
@@ -1,1 +1,21 @@
 // 최소공배수
+
+const fs = require('fs');
+const input = fs.readFileSync("example.txt").toString().trim().split(/\n/);
+// 제출시 /dev/stdin
+
+let T = parseInt(input[0]);
+
+for(let i=1; i<=T; i++) {
+    let temp = input[i].trim().split(" ").map(Number);
+    if(temp[0] >= temp[1]) console.log(lcm(temp[0], temp[1]));
+    else console.log(lcm(temp[1], temp[0]));
+}
+
+function gcd (a, b) {
+    return a % b == 0 ? b : gcd(b, a % b);
+}
+
+function lcm (a, b) {
+    return a * b / gcd(a, b);
+}
