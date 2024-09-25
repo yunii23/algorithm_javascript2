8632ec120af1ef22c98baa63f94fbac3817835bd
yunii23 <67429918+yunii23@users.noreply.github.com>
2024-09-26 01:22:10 +0900

    레벨2 숫자 변환하기 완료 (참고)

--- Programmers/Level2/js_154538.js
+++ Programmers/Level2/js_154538.js
@@ -3,5 +3,17 @@
 let x = 10;
 let y = 40;
 let n = 5;
-let answer = 0;
 
+const arr = new Array(y+1);
+arr.fill(Infinity)
+arr[x] = 0
+for(let i=x; i<=y; i++) {
+    if(arr[i] != Infinity) {
+        if(2 * i <= y) arr[2*i] = Math.min(arr[2*i], arr[i] + 1);
+        if(3 * i <= y) arr[3*i] = Math.min(arr[3*i], arr[i] + 1);
+        if(i + n <= y) arr[i+n] = Math.min(arr[i+n], arr[i] + 1);
+    }
+}
+console.log(arr);
+
+console.log(arr[y] === Infinity ? -1 : arr[y]);
