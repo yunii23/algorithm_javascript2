e621e69ac2d2a60cd59b9409d47dc1fab0f0e414
yunii23 <67429918+yunii23@users.noreply.github.com>
2024-10-03 01:12:09 +0900

    집합과 맵 - 서로 다른 부분 문자열의 개수 완료

--- Baekjoon/Set_Map/11478.js
+++ Baekjoon/Set_Map/11478.js
@@ -4,3 +4,15 @@ const fs = require('fs');
 const input = fs.readFileSync("example.txt").toString().trim();
 // 제출시 /dev/stdin
 
+const map = new Map()
+
+let i = 1;
+
+while (i <= input.length) {
+    for(let j=0; j<input.length; j++) {
+        map.set(input.substring(j, i+j))
+    }
+    i++
+}
+
+console.log(map.size);
