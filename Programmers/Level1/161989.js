141b132f0e8e034ab96960d0dfc7899e4bde815f
yunii23 <67429918+yunii23@users.noreply.github.com>
2024-10-10 19:13:19 +0900

    레벨1 - 덧칠하기 완료

--- Programmers/Level1/161989.js
+++ Programmers/Level1/161989.js
@@ -5,3 +5,17 @@ let m = 4;
 let section = [2, 3, 6];
 let answer = 0;
 
+const wall = new Array(n+1).fill(true)
+
+for(let i=0; i<section.length; i++) {
+    wall[section[i]] = false
+}
+
+for(let i=1; i<wall.length; i++) {
+    if(wall[i] == false) {
+        answer++
+        i = i + m - 1
+    }
+}
+
+console.log(answer);
