e35dcff1f1cb9b2932fdaaa4b0cd20f5d4526d52
yunii23 <67429918+yunii23@users.noreply.github.com>
2024-10-19 01:03:24 +0900

    약수, 배수와 소수 2 - 창문닫기 완료 (참고)

--- Baekjoon/Divisor_Multiple_PrimeNumber2/13909.js
+++ Baekjoon/Divisor_Multiple_PrimeNumber2/13909.js
@@ -1,1 +1,15 @@
 // 창문닫기
+
+const fs = require('fs');
+const input = fs.readFileSync("example.txt").toString().trim();
+// 제출시 /dev/stdin
+
+function open_window() {
+    let count = 0
+    for(let i=1; i*i <= parseInt(input); i++) {
+        count++
+    }
+    return count
+}
+    
+console.log(open_window(parseInt(input)));
