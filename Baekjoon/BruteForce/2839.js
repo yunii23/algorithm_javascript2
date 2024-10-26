8f2c239c9a84b1aab74618c42655d2a22a924128
yunii23 <67429918+yunii23@users.noreply.github.com>
2024-10-26 20:48:32 +0900

    브루트포스 - 설탕 배달 완료 (참고)

--- Baekjoon/BruteForce/2839.js
+++ Baekjoon/BruteForce/2839.js
@@ -4,3 +4,21 @@ const fs = require('fs');
 const input = fs.readFileSync("example.txt").toString().trim();
 // 제출시 /dev/stdin
 
+let n = parseInt(input);
+
+function sugarDelivery(n) {
+    let count = 0
+
+    while (n >= 0) {
+        if(n % 5 == 0) {
+            count += n / 5
+            return count
+        }
+        n -= 3
+        count++
+    }
+
+    return -1
+}
+
+console.log(sugarDelivery(n));
