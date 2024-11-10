5e78106fceacdd51642229bc7621ca8830b5f6bd
yunii23 <67429918+yunii23@users.noreply.github.com>
2024-11-11 03:47:18 +0900

    스택, 큐, 덱 - 제로 완료

--- Baekjoon/Stack/10773.js
+++ Baekjoon/Stack/10773.js
@@ -1,1 +1,19 @@
 // 제로
+
+const fs = require('fs');
+const input = fs.readFileSync("example.txt").toString().trim().split(/\n/);
+// 제출시 /dev/stdin
+
+const arr = input.splice(1).map(Number)
+
+const stack = []
+for(let i=0; i<arr.length; i++) {
+    if(arr[i] === 0) stack.pop()
+    else stack.push(arr[i])
+}
+
+let result = stack.reduce((acc, cur) => {
+    return acc + cur
+}, 0)
+
+console.log(result);
