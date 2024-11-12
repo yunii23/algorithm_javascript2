f2a16df7f708a5f23ac3ea806171a5b0e0b60df2
yunii23 <67429918+yunii23@users.noreply.github.com>
2024-11-12 15:48:47 +0900

    스택, 큐, 덱 - 괄호 완료

--- Baekjoon/Stack/9012.js
+++ Baekjoon/Stack/9012.js
@@ -1,1 +1,28 @@
 // 괄호
+
+const fs = require('fs');
+const input = fs.readFileSync("example.txt").toString().trim().split(/\n/);
+// 제출시 /dev/stdin
+
+const t = parseInt(input[0])
+const ps = input.splice(1)
+
+const answer = new Array(t).fill("YES")
+for(let i=0; i<ps.length; i++) {
+    const temp = ps[i].trim()
+    const stack = []
+
+    for(let j=0; j<temp.length; j++) {
+        if (temp[j] == '(') stack.push('(')
+        else {
+            if (stack.length == 0) {
+                answer[i] = "NO"
+                break
+            }
+            stack.pop()
+        }
+    }
+    if (stack.length != 0) answer[i] = "NO"
+}
+
+console.log(answer.join("\n"));
